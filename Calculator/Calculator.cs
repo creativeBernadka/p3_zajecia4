da3409adcb5c2dbc6587fb8a493481c58127918a
creativeBernadka <bfryczkowska@gmail.com>
2019-10-25 10:41:10 +0200

    first commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int AddNumbers (int first,  int second)
+        {
+            return first + second;
+        }
+        
+        public int AddNumbers (string first,  string second)
+        {
+            return Convert.ToInt32(first) + Convert.ToInt32(second);
+        }
+        
+        public int SubtractNumbers (int first,  int second)
+        {
+            return first - second;
+        }
+        
+        public int SubtractNumbers (string first,  string second)
+        {
+            return Convert.ToInt32(first) - Convert.ToInt32(second);
+        }
+        public int MultiplyNumbers (int first,  int second)
+        {
+            return first * second;
+        }
+        public int MultiplyNumbers (string first,  string second)
+        {
+            return Convert.ToInt32(first) * Convert.ToInt32(second);
+        }
+        public double DivideNumbers (int first,  int second, out bool success)
+        {
+            if (second == 0)
+            {
+                success = false;
+                return 0;
+            }
+
+            success = true;
+            return (double) first / second;
+        }
+        
+        public double DivideNumbers (string first,  string second, out bool success)
+        {
+            if (second == "0")
+            {
+                success = false;
+                return 0;
+            }
+
+            success = true;
+            return Convert.ToDouble(first) / Convert.ToDouble(second);
+        }
+
+        public double DivideNumbersWithException(int first, int second)
+        {
+            if (second == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (double) first / second;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static string ReadNumber(string number)
+        {
+            Console.WriteLine("Podaj liczbe "+number+":");
+            string text = Console.ReadLine();
+            return text;
+        }
+        
+        
+        public static void Main(string[] args)
+        {
+            string text1 = ReadNumber("1");
+            string text2 = ReadNumber("2");
+            Console.WriteLine("Wybierz operacje:");
+            string operation = Console.ReadLine();
+            int number1 = Convert.ToInt32(text1);
+            int number2 = Convert.ToInt32(text2);
+            Calculator calculator = new Calculator();
+            switch (operation) 
+            {
+                case "+":
+                    Console.WriteLine(calculator.AddNumbers(number1, number2));
+                    Console.WriteLine(calculator.AddNumbers(text1, text2));
+                    break;
+                case "-":
+                    Console.WriteLine(calculator.SubtractNumbers(number1, number2));
+                    Console.WriteLine(calculator.SubtractNumbers(text1, text2));
+                    break;
+                case "*":
+                    Console.WriteLine(calculator.MultiplyNumbers(number1, number2));
+                    Console.WriteLine(calculator.MultiplyNumbers(text1, text2));
+                    break;
+                case "/":
+                    bool success;
+                    double result = calculator.DivideNumbers(number1, number2, out success);
+                    if (success)
+                    {
+                        Console.WriteLine(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Podzielono przez zero");
+                    }
+
+                    try
+                    {
+                        Console.WriteLine(calculator.DivideNumbersWithException(number1, number2));
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Podzielono przez zero");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
